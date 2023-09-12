1730df7eb8f9c6d68db49f834ad4aa10d0f239db
semi <semilin@disroot.org>
2023-09-11 17:06:36 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
-use std::fs::File;
-#[cfg(feature = "serde")]
-use serde::{Serialize, Deserialize};
 // use rayon::prelude::*;
 
 pub type Pos = usize;
@@ -37,35 +37,39 @@ impl Corpus {
             char_map: HashMap::new(),
             char_list: char_list.clone(),
             chars: vec![0; char_list.len()],
-            bigrams: vec![0; char_list.len()*char_list.len()],
-            skipgrams: vec![0; char_list.len()*char_list.len()],
-            trigrams: vec![0; char_list.len()*char_list.len()*char_list.len()],
+            bigrams: vec![0; char_list.len() * char_list.len()],
+            skipgrams: vec![0; char_list.len() * char_list.len()],
+            trigrams: vec![0; char_list.len() * char_list.len() * char_list.len()],
         };
         for (i, chars) in c.char_list.iter().enumerate() {
-	    for ch in chars.iter() {
-		c.char_map.insert(*ch, i);
-	    }
+            for ch in chars.iter() {
+                c.char_map.insert(*ch, i);
+            }
         }
         c
     }
     pub fn uncorpus_unigram(&self, unigram: CorpusIndex) -> char {
-	self.char_list[unigram][0]
+        self.char_list[unigram][0]
     }
     pub fn uncorpus_bigram(&self, bigram: CorpusIndex) -> Vec<char> {
-	let len = self.char_list.len();
-	let c1 = bigram / len;
-	let c2 = bigram % len;
-	vec![self.char_list[c1][0], self.char_list[c2][0]]
+        let len = self.char_list.len();
+        let c1 = bigram / len;
+        let c2 = bigram % len;
+        vec![self.char_list[c1][0], self.char_list[c2][0]]
     }
     pub fn uncorpus_trigram(&self, trigram: CorpusIndex) -> Vec<char> {
-	let len = self.char_list.len();
-	let c1 = trigram / len.pow(2);
-	let c2 = trigram % len.pow(2) / len;
-	let c3 = trigram % len;
-	vec![self.char_list[c1][0], self.char_list[c2][0], self.char_list[c3][0]]
+        let len = self.char_list.len();
+        let c1 = trigram / len.pow(2);
+        let c2 = trigram % len.pow(2) / len;
+        let c3 = trigram % len;
+        vec![
+            self.char_list[c1][0],
+            self.char_list[c2][0],
+            self.char_list[c3][0],
+        ]
     }
     pub fn corpus_char(&self, c: char) -> Option<&CorpusChar> {
-	self.char_map.get(&c)
+        self.char_map.get(&c)
     }
     pub fn bigram_idx(&self, c1: CorpusChar, c2: CorpusChar) -> CorpusIndex {
         let len = self.char_list.len();
@@ -78,24 +82,24 @@ impl Corpus {
     pub fn add_str(&mut self, s: &str) {
         let mut iter = s.chars().map(|c| self.char_map.get(&c));
         // extremely gross fix later
-	let mut trigram: Vec<Option<&CorpusChar>> = vec![None, None, None];
-	while let Some(c) = iter.next() {
-	    trigram.rotate_left(1);
-	    trigram[2] = c;
-	    if let Some(c3) = trigram[2] {
-		self.chars[*c3] += 1;
-		if let Some(c2) = trigram[1] {
-		    let bg = self.bigram_idx(*c2, *c3);
-		    self.bigrams[bg] += 1;
-		    if let Some(c1) = trigram[0] {
-			let tg = self.trigram_idx(*c1, *c2, *c3);
-			self.trigrams[tg] += 1;
-			let sg = self.bigram_idx(*c1, *c3);
-			self.skipgrams[sg] += 1;
-		    }
-		}
-	    }
-	}
+        let mut trigram: Vec<Option<&CorpusChar>> = vec![None, None, None];
+        while let Some(c) = iter.next() {
+            trigram.rotate_left(1);
+            trigram[2] = c;
+            if let Some(c3) = trigram[2] {
+                self.chars[*c3] += 1;
+                if let Some(c2) = trigram[1] {
+                    let bg = self.bigram_idx(*c2, *c3);
+                    self.bigrams[bg] += 1;
+                    if let Some(c1) = trigram[0] {
+                        let tg = self.trigram_idx(*c1, *c2, *c3);
+                        self.trigrams[tg] += 1;
+                        let sg = self.bigram_idx(*c1, *c3);
+                        self.skipgrams[sg] += 1;
+                    }
+                }
+            }
+        }
     }
     pub fn add_file<P: AsRef<Path>>(&mut self, path: P) -> io::Result<()> {
         let file = File::open(path)?;
@@ -129,10 +133,16 @@ impl Layout {
                 let idx = corpus.bigram_idx(self.matrix[idx[0]], self.matrix[idx[1]]);
                 match ng {
                     Some(NgramType::Skipgram) => corpus.skipgrams[idx],
-                    _ => corpus.bigrams[idx]
+                    _ => corpus.bigrams[idx],
                 }
-            },
-            Nstroke::Tristroke(idx) => corpus.trigrams[corpus.trigram_idx(self.matrix[idx[0]], self.matrix[idx[1]], self.matrix[idx[2]])],
+            }
+            Nstroke::Tristroke(idx) => {
+                corpus.trigrams[corpus.trigram_idx(
+                    self.matrix[idx[0]],
+                    self.matrix[idx[1]],
+                    self.matrix[idx[2]],
+                )]
+            }
         }
     }
     pub fn total_char_count(&self, corpus: &Corpus) -> u32 {
@@ -140,9 +150,7 @@ impl Layout {
     }
 }
 
-#[cfg_attr(feature = "serde",
-    derive(Serialize, Deserialize),
-    serde(untagged))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize), serde(untagged))]
 #[derive(Debug)]
 pub enum Nstroke {
     Monostroke(usize),
@@ -188,9 +196,7 @@ pub struct NstrokeData {
 
 impl NstrokeData {
     pub fn new(nstroke: Nstroke, amounts: Vec<MetricAmount>) -> Self {
-        Self {
-            nstroke, amounts
-        }
+        Self { nstroke, amounts }
     }
 }
 
@@ -227,7 +233,9 @@ impl MetricData {
             }
         }
         Self {
-            metrics, strokes, position_strokes
+            metrics,
+            strokes,
+            position_strokes,
         }
     }
 }
@@ -243,26 +251,31 @@ impl Analyzer {
     pub fn from(data: MetricData, corpus: Corpus, layout: Layout) -> Self {
         let mut stats: Vec<f32> = vec![0.0; data.metrics.len()];
 
-	for stroke in &data.strokes {
-	    let ns = &stroke.nstroke;
-	    let basefreq = layout.frequency(&corpus, ns, None);
+        for stroke in &data.strokes {
+            let ns = &stroke.nstroke;
+            let basefreq = layout.frequency(&corpus, ns, None);
             let skipfreq = match ns {
-                Nstroke::Bistroke(_) => Some(layout.frequency(&corpus, ns, Some(NgramType::Skipgram))),
-                _ => None
+                Nstroke::Bistroke(_) => {
+                    Some(layout.frequency(&corpus, ns, Some(NgramType::Skipgram)))
+                }
+                _ => None,
             };
 
-	    for amount in &stroke.amounts {
+            for amount in &stroke.amounts {
                 let freq = if let NgramType::Skipgram = data.metrics[amount.metric] {
                     skipfreq.unwrap()
                 } else {
                     basefreq
                 };
-		stats[amount.metric] += freq as f32 * amount.amount;
-	    }
-	}
-	
-	Self {
-	    data, corpus, layouts: vec![layout], stats
-	}
+                stats[amount.metric] += freq as f32 * amount.amount;
+            }
+        }
+
+        Self {
+            data,
+            corpus,
+            layouts: vec![layout],
+            stats,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use keycat::{Nstroke, NgramType, NstrokeData, MetricAmount, MetricData, Corpus, Analyzer, Layout};
+use keycat::{Analyzer, Corpus, Layout, MetricAmount, MetricData, NgramType, Nstroke, NstrokeData};
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 
 pub fn main() {
     let mut corpus = {
@@ -8,36 +8,44 @@ pub fn main() {
             .chars()
             .map(|c| vec![c, c.to_uppercase().next().unwrap()])
             .collect::<Vec<Vec<char>>>();
-        char_list.extend(vec![vec![',', '<'],
-			      vec!['.', '>'],
-			      vec!['/', '?'],
-			      vec!['\'', '\"']]);
+        char_list.extend(vec![
+            vec![',', '<'],
+            vec!['.', '>'],
+            vec!['/', '?'],
+            vec!['\'', '\"'],
+        ]);
         Corpus::with_char_list(char_list)
     };
 
     println!("{:?}", corpus.char_list);
 
-    corpus.add_file("./tr.txt").unwrap();
+    corpus
+        .add_file("./iweb-corpus-samples-cleaned.txt")
+        .unwrap();
 
     for (i, v) in corpus.bigrams.iter().enumerate() {
-	if *v >= 533 {
-	    let s: String = corpus.uncorpus_bigram(i).iter().collect();
-	    println!("{}. '{}' {}", i, s, v);
-	}
+        if *v >= 533 {
+            let s: String = corpus.uncorpus_bigram(i).iter().collect();
+            println!("{}. '{}' {}", i, s, v);
+        }
     }
 
     let metrics = vec![NgramType::Bigram, NgramType::Skipgram];
     let strokes = vec![
         NstrokeData::new(Nstroke::Bistroke([0, 13]), vec![MetricAmount::new(0, 1.0)]),
-        NstrokeData::new(Nstroke::Bistroke([0, 13]), vec![MetricAmount::new(1, 1.0)])];
+        NstrokeData::new(Nstroke::Bistroke([0, 13]), vec![MetricAmount::new(1, 1.0)]),
+    ];
     let data = MetricData::from(metrics, strokes, 30);
     println!("{:?}", data.position_strokes);
 
     let layout = Layout {
-	matrix: "flhvz'wuoysrntkcdeaixjbmqpg,./".chars().map(|c| *corpus.corpus_char(c).unwrap()).collect()
+        matrix: "flhvz'wuoysrntkcdeaixjbmqpg,./"
+            .chars()
+            .map(|c| *corpus.corpus_char(c).unwrap())
+            .collect(),
     };
-    
-    let analyzer = Analyzer::from(&data, &corpus, layout);
+
+    let analyzer = Analyzer::from(data, corpus, layout);
 
     println!("{:?}", analyzer.stats);
 }
