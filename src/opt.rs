762777b949a81ec25f0cc983b44d934f6a5f79a9
semi <semilin@disroot.org>
2024-04-03 13:42:18 -0700

    refactor optimization module, add tests

--- src/opt.rs
+++ src/opt.rs
@@ -2,16 +2,39 @@ use crate::{analysis::Analyzer, Layout, Swap};
 use rand::prelude::*;
 use rayon::prelude::*;
 
-pub struct Scoring {
-    pub weights: Vec<(usize, f32)>,
+/// Trait for objective functions, used in optimization.
+pub trait Objective {
+    /// Returns how well the stats meet the objective. Lower values
+    /// should mean a better fit to the objective.
+    fn score(&self, stats: &[f32]) -> f32;
 }
 
-impl Scoring {
+pub struct Weight {
+    metric: usize,
+    weight: f32,
+}
+
+/// The most basic kind of objective function. Each metric is
+/// associated with a multiplicative weight to create a single
+/// composite value.
+pub struct WeightsObjective {
+    pub weights: Vec<Weight>,
+}
+
+impl WeightsObjective {
+    pub fn new(weights: Vec<Weight>) -> Self {
+        WeightsObjective { weights }
+    }
+}
+
+impl Objective for WeightsObjective {
     #[must_use]
-    pub fn score(&self, diff: &[f32]) -> f32 {
+    fn score(&self, stats: &[f32]) -> f32 {
         self.weights
             .iter()
-            .fold(0.0, |acc, (i, weight)| acc + (weight * diff[*i]))
+            .fold(0.0, |acc, Weight { metric, weight}| {
+                acc + (weight * stats[*metric])
+            })
     }
 }
 
@@ -21,7 +44,11 @@ pub trait Optimizer {
     #[must_use]
     fn pin(self, pins: Vec<usize>) -> Self;
     /// Runs the optimization for as long as needed.
-    fn run(&mut self, analyzer: &Analyzer, scoring: &Scoring) -> Vec<(Layout, f32)>;
+    fn run(
+        &mut self,
+        analyzer: &Analyzer,
+        objective: &(dyn Objective + Send + Sync),
+    ) -> Vec<(Layout, f32)>;
 }
 
 /// An `Optimizer` that runs simulated annealing on a pool of size
@@ -57,7 +84,11 @@ impl Optimizer for AnnealingOptimizer {
         self
     }
 
-    fn run(&mut self, analyzer: &Analyzer, scoring: &Scoring) -> Vec<(Layout, f32)> {
+    fn run(
+        &mut self,
+        analyzer: &Analyzer,
+        objective: &(dyn Objective + Send + Sync),
+    ) -> Vec<(Layout, f32)> {
         self.layouts.par_iter_mut().for_each(|l| {
             let mut diffs = vec![0.0; analyzer.data.metrics.len()];
             let mut rng = rand::thread_rng();
@@ -74,7 +105,7 @@ impl Optimizer for AnnealingOptimizer {
                     .choose(&mut rng)
                     .expect("possible_swaps should not be empty");
                 analyzer.swap_diff(&mut diffs, l, swap);
-                let diff = scoring.score(&diffs);
+                let diff = objective.score(&diffs);
                 if diff < 0.0 || rng.gen::<f64>() < temp {
                     l.swap(swap);
                 }
@@ -87,9 +118,58 @@ impl Optimizer for AnnealingOptimizer {
         let mut layouts: Vec<(Layout, f32)> = self
             .layouts
             .par_iter()
-            .map(|l| (l.clone(), scoring.score(&analyzer.calc_stats(l))))
+            .map(|l| (l.clone(), objective.score(&analyzer.calc_stats(l))))
             .collect();
         layouts.sort_by(|a, b| a.1.partial_cmp(&b.1).expect("score should never be NaN"));
         layouts
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        analysis::{Analyzer, MetricAmount, MetricData, NstrokeData},
+        Corpus, NgramType, Nstroke,
+    };
+    #[test]
+    fn test_optimization() {
+        let mut corpus = Corpus::with_char_list(
+            "abcdefghijklmnopqrstuvwxyz,./;"
+                .chars()
+                .map(|c| vec![c])
+                .collect(),
+        );
+
+        let text = "the quick brown fox jumps over the lazy dog";
+        corpus.add_str(&text);
+
+        let qwerty = corpus.layout_from_str("qazwsxedcrfvtgbyhnujmik,lo.p;/");
+
+        let metrics = vec![NgramType::Bigram];
+        let mut strokes: Vec<NstrokeData> = vec![];
+        // bigram alternation
+        for a in 0..30 {
+            for b in 0..30 {
+                if (a < 15) == (b < 15) {
+                    strokes.push(NstrokeData::new(
+                        Nstroke::Bistroke([a, b]),
+                        vec![MetricAmount::new(0, 1.0)],
+                    ));
+                }
+            }
+        }
+        let data = MetricData::from(metrics, strokes, 30);
+        let analyzer = Analyzer::from(data, corpus);
+        let mut optimizer = AnnealingOptimizer::new(8, -0.01).pin(vec![0]);
+        let objective = WeightsObjective::new(vec![Weight {
+            metric: 0,
+            weight: 1.0,
+        }]);
+        let start = objective.score(&analyzer.calc_stats(&qwerty));
+        optimizer.setup(qwerty.clone());
+        let optimized = optimizer.run(&analyzer, &objective);
+        let end = &optimized[0].1;
+        assert!(*end < start, "optimized should be lower score than unoptimized");
     }
 }
